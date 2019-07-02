c72da64c9c6a02639a18d5eb9cfdfca324eecfce
MarieWeiel <marie.weiel@kit.edu>
2019-07-02 16:55:24 +0200

    added explaining comments to DynamicPSOSolver.py

--- hyppopy/solvers/DynamicPSOSolver.py
+++ hyppopy/solvers/DynamicPSOSolver.py
@@ -20,25 +20,26 @@ LOG = logging.getLogger(os.path.basename(__file__))
 LOG.setLevel(DEBUGLEVEL)
 
 from hyppopy.solvers.HyppopySolver import HyppopySolver
+from . import OptunitySolver
 
-
-class OptunitySolver(HyppopySolver):
+class DynamicPSOSolver(OptunitySolver):
 
     def __init__(self, project=None):
         """
-        The constructor accepts a HyppopyProject.
+        The OptunitySolver constructor accepts a HyppopyProject.
 
         :param project: [HyppopyProject] project instance, default=None
         """
-        HyppopySolver.__init__(self, project)
+        super().__init__(self,project)
+        #HyppopySolver.__init__(self, project)
 
     def define_interface(self):
         """
         This function is called when HyppopySolver.__init__ function finished. Child classes need to define their
-        individual parameter here by calling the _add_member function for each class member variable need to be defined.
-        Using _add_hyperparameter_signature the structure of a hyperparameter the solver expects must be defined.
-        Both, members and hyperparameter signatures are later get checked, before executing the solver, ensuring
-        settings passed fullfill solver needs.
+        individual parameters here by calling _add_member function for each class member variable to be defined.
+        Using _add_hyperparameter_signature, the structure of a hyperparameter expected by the solver must be defined.
+        Both members and hyperparameter signatures are checked later on, before executing the solver, ensuring
+        settings passed fulfil the solver's needs.
         """
         self._add_member("max_iterations", int)
         self._add_hyperparameter_signature(name="domain", dtype=str,
@@ -64,7 +65,7 @@ class OptunitySolver(HyppopySolver):
 
     def execute_solver(self, searchspace):
         """
-        This function is called immediately after convert_searchspace and get the output of the latter as input. It's
+        This function is called immediately after convert_searchspace and uses the output of the latter as input. Its
         purpose is to call the solver libs main optimization function.
 
         :param searchspace: converted hyperparameter space
@@ -74,6 +75,35 @@ class OptunitySolver(HyppopySolver):
             self.best, _, _ = optunity.minimize_structured(f=self.loss_function,
                                                            num_evals=self.max_iterations,
                                                            search_space=searchspace)
+            """
+            In Optunity api.py: optunity.minimze_structured(f,search_space,num_evals=50,pmap=map)
+            Basic function minimization routine minimizing f within given box constraints.
+            
+            :param f: function to be minimized
+            :param search_space: search space
+            :param num_evals: number of permitted function evaluations
+            :param pmap: callable map function to use
+            
+            :return: retrieved minimum, extra info, solver info
+            
+            This function will implicitly choose an appropriate solver based on "num_evals" and box constraints.
+            Set up tree structure to model search space.
+            Create set of box constraints to define given search space.
+            Suggest solver to use (default: PSO).
+            Create solver from given parameters.
+            Use 'optimize' function for respective solver requested:
+            optimize(solver,f,maximize=False,max_evals=num_evals,pmap=pmap,decoder=tree.decode)
+            In general: optimize(solver,func,maximize=True,max_evals=0,pmap=map,decoder=None)
+            Optimize func with given solver.
+            :param solver: solver to be used, e.g. result from :func: optunity.make_solver
+            :param func: [callable] objective function
+            :param maximize: [bool] maximize or minimize
+            :param max_evals: [int] maximum number of permitted function evaluations
+            :param pmap: [function] map() function to use
+            
+            :return: solution, named tuple with further details
+            optimize function (api.py) internally uses optiize function from requested solver module.
+            """
         except Exception as e:
             LOG.error("internal error in optunity.minimize_structured occured. {}".format(e))
             raise BrokenPipeError("internal error in optunity.minimize_structured occured. {}".format(e))
